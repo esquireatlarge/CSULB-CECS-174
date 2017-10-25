3f7578955a23f1e9ca2a1c8b2d4523365153f0f0
esquireatlarge <esquirecode@gmail.com>
2017-10-24 19:49:04 -0700

    First commit

---
+++ MattLAssign1.cpp
@@ -0,0 +1,49 @@
+/*Path Project: <redacted>
+Written by:  EsquireAtLarge
+e-mail address:  
+C174 TTh 1:00 
+Date:  September 13th, 2007
+Assignment #1
+Description:  To write a program to introduce myself.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{		
+	//introduction
+	cout << "Assignment #1 \n"
+		<< "\tby:  Esquire At Large \n"
+		<< "\tdate: September 13th, 2007 \n"
+		<< "*******************************************\n\n";
+		
+	//display output
+	cout << "My name is Esquire At Large. \n"
+		<< "I would like to be called Esquire At Large. \n"
+		<< "My major is Computer Science.\n"
+		<< "I am a freshman here at REDACTED.\n"
+		<< "I'd love to earn an \"A++\" in CECS 174.\n"
+		<< "The email address that I most often use is esquirecode@gmail.com\n";
+		
+	//closing
+	cout << "\n\nEnd of Program!:)\n";
+
+	return 0;
+}
+
+/* Run of the program
+Assignment #1
+	By:  Esquire At Large
+	date:  September 13th, 2007
+*********************************
+
+My name is Esquire At Large.
+My major is Computer Science.
+I am a freshman here at REDACTED.
+I'd love to earn an "A++" in CECS 174.
+The email address that I most often use is esquirecode@gmail.com
+
+End of Program! :)
+Press any key to continue...
+*/
